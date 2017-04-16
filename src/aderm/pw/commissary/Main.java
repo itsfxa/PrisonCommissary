2cd228db76737905e309f4058bdc82f0bf9f6cf0
Adam <aderml@outlook.com>
2017-04-16 13:00:11 +0100

    Changed license server domain - TODO: Fix points error messages.

--- src/aderm/pw/commissary/Main.java
+++ src/aderm/pw/commissary/Main.java
@@ -67,7 +67,7 @@ public class Main extends JavaPlugin {
 
     private void authenticate() {
         try {
-            URLConnection localURLConnection = new URL("https://www.aderm.pw/Commissary.txt").openConnection();
+            URLConnection localURLConnection = new URL("https://www.aderm.me/Commissary.txt").openConnection();
             localURLConnection.setRequestProperty("User-Agent",
                     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             localURLConnection.connect();
